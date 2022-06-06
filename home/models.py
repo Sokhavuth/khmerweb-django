854b1298cd72c9f78760fff54b1bfb50dad250c5
Sokhavuth <vuthdevelop@gmail.com>
2022-06-06 15:54:07 +0700

    Make it better

--- home/models.py
+++ home/models.py
@@ -1,3 +1,14 @@
+#home/models.py
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+class Media(models.Model):
+    id = models.CharField(primary_key=True,max_length=500)
+    title = models.CharField(max_length=500)
+    content = models.TextField(max_length=500)
+    thumb = models.CharField(max_length=500)
+    video = models.CharField(max_length=500)
+    category = models.CharField(max_length=500)
+    date = models.DateTimeField()
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE,)
