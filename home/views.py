f1b68cdf71badb91659633f869c3837a6b1f609b
Sokhavuth <vuthdevelop@gmail.com>
2022-06-07 19:48:06 +0700

    Make it better

--- home/views.py
+++ home/views.py
@@ -4,7 +4,9 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     kdict = {
-        'siteTitle':'Khmer Web'
+        'siteTitle':'Khmer Web',
+        'pageTitle': 'Home',
+        'message': 'ស្វាគមន៍​មក​កាន់​ទំព័រ​ដើម!'
     }
     
     return render(request,'base.html',context={'data':kdict})
